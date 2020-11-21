cb553fb9b1870952a0af8f30c8acf668292d9e2f
April Howard <apehow111@gmail.com>
2020-11-21 16:20:59 -0500

    start of TicTacToe program

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    public String board[][];
+
+    public TicTacToe(String[][] board) {
+        this.board = board;
+
     }
 
+    public TicTacToe() {
 
+    }
+}
 
 
 
